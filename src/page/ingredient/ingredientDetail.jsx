4bc99b7c532ffb16886411bc72d4e5ad4a822fad
Lawrence Fong <lawrencehei@hotmail.com>
2024-01-24 17:49:46 +0800

    update ingredientType

--- src/page/ingredient/ingredientDetail.jsx
+++ src/page/ingredient/ingredientDetail.jsx
@@ -27,6 +27,7 @@ export const IngredientDetail = () => {
     <div>
       <h2>{ingredient.ingredientName}</h2>
       <img  className="thumbnail" src={ingredient.ingredientPhoto} alt={ingredient.ingredientName} />
+      <p>類型: {ingredient.ingredientType}</p>
       <p>重量: {ingredient.ingredientWeight}</p>
       <p>價格: {ingredient.ingredientPrice} 元</p>
       <p>價格/重量: {calculatePricePerWeight()} 元/(克/毫升/粒/盒)</p>
